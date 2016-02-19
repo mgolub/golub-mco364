ca731aec32a3e3a0cc2edbc9dbb9e29945422de0
Malkie Golub <Malkie Golub>
2016-02-18 19:40:59 -0500

    updated paint program

--- src/main/java/golub/mco364/paint/Canvas.java
+++ src/main/java/golub/mco364/paint/Canvas.java
@@ -1,8 +1,6 @@
 package golub.mco364.paint;
 
-import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -13,79 +11,65 @@ import javax.swing.JPanel;
 public class Canvas extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private BufferedImage buffer;
-	private Point start;
-	private Point end;
-	
-	
-	
-	
-	public Canvas(){
-		
-		this.buffer = new BufferedImage(800,800,BufferedImage.TYPE_INT_ARGB);
-		start = new Point();
-		end = new Point();
-		
-		
-		this.addMouseListener(new MouseListener(){
+	private Tool tool;
+
+	public Canvas() {
+
+		this.buffer = new BufferedImage(800, 800, BufferedImage.TYPE_INT_ARGB);
+		this.tool = new LineTool();
+
+		this.addMouseListener(new MouseListener() {
 
 			public void mouseClicked(MouseEvent event) {
-				
+
 			}
 
 			public void mouseEntered(MouseEvent event) {
-				
+
 			}
 
 			public void mouseExited(MouseEvent event) {
-				
+
 			}
 
 			public void mousePressed(MouseEvent event) {
-				start = event.getPoint();
-				
+				tool.mousePressed(buffer.getGraphics(), event.getX(), event.getY());
+				repaint();
+
 			}
 
 			public void mouseReleased(MouseEvent event) {
-				
+				tool.mouseReleased(buffer.getGraphics(), event.getX(), event.getY());
+				repaint();
+
 			}
-			
+
 		});
-		
-		this.addMouseMotionListener(new MouseMotionListener(){
+
+		this.addMouseMotionListener(new MouseMotionListener() {
 
 			public void mouseDragged(MouseEvent event) {
-				
-				Graphics g = buffer.getGraphics();
-				g.setColor(Color.RED);
-				
-				end = event.getPoint();
-				int startX = (int) start.getX();
-				int startY = (int) start.getY();
-				int endX = (int) end.getX();
-				int endY = (int) end.getY();
-				g.drawLine(startX, startY, endX, endY);
+
+				tool.mouseDragged(buffer.getGraphics(), event.getX(), event.getY());
 				repaint();
-				start = end;
 			}
 
 			public void mouseMoved(MouseEvent event) {
-				
+
 			}
-			
+
 		});
 	}
-	
-	@Override 
-	protected void paintComponent(Graphics g){
+
+	@Override
+	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		
+
 		g.drawImage(buffer, 0, 0, null);
-		
-		
-		
+		tool.drawPreview(g);
+
 	}
 
 }
---
+++ src/main/java/golub/mco364/paint/PencilTool.java
@@ -0,0 +1,41 @@
+package golub.mco364.paint;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class PencilTool implements Tool{
+	
+	private int x;
+	private int y;
+
+	public void mousePressed(Graphics g, int x, int y) {
+		g.setColor(Color.RED);
+		g.fillOval(x,y,1,1);
+		this.x = x;
+		this.y = y;
+		
+	}
+
+	public void mouseReleased(Graphics g, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseDragged(Graphics g, int x, int y) {
+		
+		
+		g.setColor(Color.RED);
+		g.drawLine(this.x, this.y, x, y);
+		this.x = x;
+		this.y = y;
+		
+	}
+
+	public void drawPreview(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
