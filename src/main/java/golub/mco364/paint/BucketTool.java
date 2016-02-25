68466f5bf89033cf24bb74a99958ebe82491419b
Malkie Golub <Malkie Golub>
2016-02-25 16:51:51 -0500

    updated paint

---
+++ src/main/java/golub/mco364/paint/BucketTool.java
@@ -0,0 +1,27 @@
+package golub.mco364.paint;
+
+import java.awt.Graphics;
+
+public class BucketTool implements Tool{
+
+	public void mousePressed(Graphics g, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(Graphics g, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseDragged(Graphics g, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void drawPreview(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
