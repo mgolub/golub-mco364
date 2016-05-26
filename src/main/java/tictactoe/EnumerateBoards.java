0be0c3440a6c0774cbe875d522f8307c6527052a
Malkie Golub <Malkie Golub>
2016-05-26 18:20:42 -0400

    updated tictactoe

--- src/main/java/tictactoe/EnumerateBoards.java
+++ src/main/java/tictactoe/EnumerateBoards.java
@@ -9,40 +9,6 @@ public class EnumerateBoards {
 	 * Determine number of possible boards and which piece wins more often, X,
 	 * O. Find how many times each piece wins.
 	 */
-
-//	private int numBoards = 0;
-//	private int Xwins = 0;
-//	private int Owins = 0;
-//
-//	public EnumerateBoards() {
-//		Board empty = new Board();
-//		enumerate(empty);
-//	}
-//
-//	public void enumerate(Board b) {
-//		// need to know current player, number of empty spaces...
-//		Piece active = b.getActivePlayer();
-//		int emptySpaces = b.getMoves().size();
-//		for (int i = 0; i < emptySpaces; i++) {
-//			if (b.getWinner() != null) {
-//				increaseWins(b.getWinner());
-//				return;
-//			} else {
-//
-//			}
-//
-//			enumerate(new Board(b));
-//			numBoards++;
-//		}
-//	}
-//
-//	private void increaseWins(Piece p) {
-//		if (p == Piece.X)
-//			Xwins++;
-//		else
-//			Owins++;
-//	}
-
 	public static void main(String[] args){
 		Stack<Board> stack = new Stack<Board>();
 		int numBoards = 0;
